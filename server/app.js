982410cbc2614b9a759890df00dff7e40ad49c05
Aakarsh Kashyap <aakarshkashyap143341@gmail.com>
2026-07-01 15:33:43 +0530

    feat: connect mongodb atlas

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import healthRoutes from "./routes/health.routes.js";
 
 const app = express();
 
@@ -9,12 +10,16 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use("/api/health", healthRoutes);
+
 // Temporary Route
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Welcome to CodeSage AI API 🚀",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.json({
+//     success: true,
+//     message: "Welcome to CodeSage AI API 🚀",
+//   });
+// });
+
+
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+// Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
